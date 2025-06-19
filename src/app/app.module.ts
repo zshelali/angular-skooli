2772f4ca6746fe446800c46a3fa12991956efac5
zshelali <ali.hajeri24@gmail.com>
2025-06-19 18:01:16 +0200

    Remove duplicate import of HttpClientModule in app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,7 +11,6 @@ import { ProfileComponent } from './profile/profile.component';
 import { ModuleUEComponent } from './module-ue/module-ue.component';
 import { ModuleUEListComponent } from './module-ue-list/module-ue-list.component';
 import {UeComponent} from "./ue/ue.component";
-import { HttpClientModule } from '@angular/common/http';
 
 
 import { HttpClientModule } from '@angular/common/http';
