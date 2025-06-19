b27a1fc26af1fbde42cf1170bab05e67eb4f96f4
zshelali <ali.hajeri24@gmail.com>
2025-06-19 17:49:44 +0200

    Refactor server.js for consistent quotation marks and formatting

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,8 @@
-const express = require('express');
-const cors = require('cors');
-require('dotenv').config({ path: __dirname + '/.env' }); //path makes it so that node server.js works from anywhere
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config({ path: __dirname + "/.env" }); //path makes it so that node server.js works from anywhere
 
-const { connectToMongoDB } = require('./db');
+const { connectToMongoDB } = require("./db");
 
 const app = express();
 
@@ -10,17 +10,19 @@ app.use(express.json());
 app.use(cors());
 
 connectToMongoDB()
-    .then(() => {
-        app.use('/api/modules', require('./routes/module.route'));
-        app.use('/api/secret', require('./routes/protected.route'));
-        app.use('/api/auth', require('./routes/auth.route'));
-        app.use('/api/users', require('./routes/user.route'));
+  .then(() => {
+    app.use("/api/modules", require("./routes/module.route"));
+    app.use("/api/secret", require("./routes/protected.route"));
+    app.use("/api/auth", require("./routes/auth.route"));
+    app.use("/api/users", require("./routes/user.route"));
 
-        const PORT = process.env.PORT || 3000;
-        app.listen(PORT, () => {
-            console.log(`Server running on port ${PORT}.🗡️`);
-        });
-    })
-    .catch(err => {
-        console.error(`Failed to connect to MongoDB😢: ${err}`);
+    const PORT = process.env.PORT || 3000;
+    app.listen(PORT, () => {
+      console.log(`Server running on port ${PORT}.🗡️`);
     });
+  })
+  .catch((err) => {
+    console.error(
+      `Failed to connect to MongoDB😢 (You probably have a problem with your .env): ${err}`
+    );
+  });
